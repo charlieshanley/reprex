d6ddee5dd1b17d12db3002f3163514b0232cfa53
Charlie Hanley <chanley@interos.net>
2019-05-09 10:33:25 -0400

    add bootstrap and own css

--- frontend/src/Frontend.hs
+++ frontend/src/Frontend.hs
@@ -12,10 +12,18 @@ import Common.Api
 import Common.Route
 import Obelisk.Generated.Static
 
-
 frontend :: Frontend (R FrontendRoute)
 frontend = Frontend
-  { _frontend_head = el "title" $ text "Obelisk Minimal Example"
+  { _frontend_head = do
+      el "title" $ text "Obelisk Minimal Example"
+      let bootstrap = "rel" =: "stylesheet"
+                   <> "href" =: "https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+                   <> "integrity" =: "sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
+                   <> "crossorigin" =: "anonymous"
+          myCss = "rel" =: "stylesheet"
+               <> "href" =: (static @"styles.css")
+      elAttr "link" bootstrap blank
+      elAttr "link" myCss blank
   , _frontend_body = do
       text "Welcome to Obelisk!"
       el "p" $ text $ T.pack commonStuff
